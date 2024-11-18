4b77ed2512271a008186e5ae9e34632c66cb8073
robertomalatestale <robertomalatestale@gmail.com>
2024-11-18 20:06:44 -0300

    Create method to check remaining cards in the deck

--- src/main/java/org/aplicacao/DeckOfCards.java
+++ src/main/java/org/aplicacao/DeckOfCards.java
@@ -1,5 +1,25 @@
 package org.aplicacao;
 
-public class DeckOfCards {
+import org.aplicacao.service.APIService;
 
+public class DeckOfCards extends APIService {
+    protected String deckId;
+    private int remaining;
+
+    public String getDeckId() {
+        return deckId;
+    }
+
+    protected String createDeckId() throws Exception {
+        this.deckId = createDeck();
+        return this.deckId;
+    }
+
+    public int getRemaining() {
+        return remaining;
+    }
+
+    public void setRemaining(int remaining) {
+        this.remaining = remaining;
+    }
 }
