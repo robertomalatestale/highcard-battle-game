b12deb290c7d50d7be66eff04ecaba7a93b6a7ac
robertomalatestale <robertomalatestale@gmail.com>
2024-11-18 20:02:02 -0300

    Introduce new methods to fulfill player hand

--- src/main/java/org/aplicacao/Player.java
+++ src/main/java/org/aplicacao/Player.java
@@ -1,11 +1,45 @@
 package org.aplicacao;
 
-public class Player {
+import org.aplicacao.dto.DtoCardResponse;
+import org.aplicacao.service.APIService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player extends APIService {
     private String name;
     private int matchesWon;
-    private String[] hand = new String[5];
+    private List<DtoCardResponse> hand = new ArrayList<>();
 
     public Player(String name) {
         this.name = name;
+    }
+
+    public void fulfillHand(String deckId) throws Exception {
+        this.hand = fulfillPlayerHand(deckId).getCards();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMatchesWon() {
+        return matchesWon;
+    }
+
+    public void setMatchesWon(int matchesWon) {
+        this.matchesWon = matchesWon;
+    }
+
+    public List<DtoCardResponse> getHand() {
+        return hand;
+    }
+
+    public void setHand(List<DtoCardResponse> hand) {
+        this.hand = hand;
     }
 }
