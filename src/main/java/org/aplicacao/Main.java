41864bb0270692d6c1ae31112b0fa03b0a381be1
robertomalatestale <robertomalatestale@gmail.com>
2024-12-05 20:09:08 -0300

    Made the card drawn and discard logic

--- src/main/java/org/aplicacao/Main.java
+++ src/main/java/org/aplicacao/Main.java
@@ -1,28 +1,15 @@
 package org.aplicacao;
 
-
-import org.aplicacao.dto.DtoDeckResponse;
-import org.aplicacao.service.APIService;
-
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         DeckOfCards deck = new DeckOfCards();
         Player player1 = new Player("Roberto");
-        Player player2 = new Player("Davi");
-        Player player3 = new Player("Allan");
-        try{
-            String deckId = deck.createDeckId();
-            player1.fulfillHand(deckId);
-            System.out.println(player1.getHand());
-            System.out.println(deck.checkRemainingCards(deckId));
-            player2.fulfillHand(deckId);
-            System.out.println(player2.getHand());
-            System.out.println(deck.checkRemainingCards(deckId));
-            player3.fulfillHand(deckId);
-            System.out.println(player3.getHand());
-            System.out.println(deck.checkRemainingCards(deckId));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String deckId = deck.createDeckId();
+        player1.fulfillHand(deckId,3);
+        player1.playerTurn(deckId);
+        player1.playerTurn(deckId);
+        player1.playerTurn(deckId);
     }
 }
+
+//TODO Começar a pensar na lógica do jogo agora, qual carta vence qual
--- src/main/java/org/aplicacao/Player.java
+++ src/main/java/org/aplicacao/Player.java
@@ -11,31 +11,68 @@ public class Player extends APIService {
     private String name;
     private int matchesWon;
     private List<DtoCardResponse> hand = new ArrayList<>();
-    static private int discardCount;
+    static private int discardCount = 1;
     Scanner scanner = new Scanner(System.in);
 
     public Player(String name) {
         this.name = name;
     }
 
-    public void fulfillHand(String deckId) throws Exception {
-        this.hand = drawCards(deckId,3).getCards();
+    public void fulfillHand(String deckId, int quantity) throws Exception {
+        this.hand = drawCards(deckId,quantity).getCards();
+    }
+    public void addCardToHand(DtoCardResponse card) {
+        hand.add(card);
     }
 
-    public void playerTurn(){
-        System.out.println("Choose a option, if you want to play a card choose between 1 to 3.");
+    public void playerTurn(String deckId) throws Exception {
+        System.out.println("Choose an option! If you want to play a card choose between 1 to 3.");
         System.out.println("Or if you want to discard one card to draw another from the deck, choose option 4 (only 1 discard per match)");
         for(int i = 0; i<hand.size();i++){
             System.out.println((i+1) + " - " + hand.get(i));
         }
-        System.out.println("4 - Discard a card.");
+        System.out.println("4 - Discard a card");
         System.out.print("Enter your choice here: ");
         int playerChoice = scanner.nextInt();
-        while (playerChoice>4 || playerChoice<=0){
-            System.out.println("Invalid value!");
+        while (playerChoice==4 && discardCount!=1){
+            System.out.println("You are out of discards! Choose another option.");
             System.out.print("Enter your choice here: ");
             playerChoice = scanner.nextInt();
         }
+        while (playerChoice>4 || playerChoice<=0){
+            System.out.println("Invalid value!");
+            System.out.print("Enter your choice here: ");
+            playerChoice = scanner.nextInt();}
+        switch(playerChoice){
+            case 1, 2, 3:
+                System.out.println("\n" + this.name + " played the card: " + hand.get(playerChoice-1 ) + "\n");
+                discardCard(hand,(playerChoice));
+                DtoCardResponse cardDrawn = new DtoCardResponse();
+                List<DtoCardResponse> oneCardDrawn = new ArrayList<>();
+                oneCardDrawn = drawCards(deckId,1).getCards();
+                cardDrawn = oneCardDrawn.getFirst();
+                addCardToHand(cardDrawn);
+                break;
+            case 4:
+                discardCount -= 1;
+                System.out.println("Choose an option from 1 to 3 to select which card to discard");
+                for(int i = 0; i<hand.size();i++){
+                    System.out.println((i+1) + " - " + hand.get(i));
+                }
+                System.out.print("Choose your card: ");
+                playerChoice = scanner.nextInt();
+                while (playerChoice>3 || playerChoice<=0){
+                    System.out.println("Invalid value!");
+                    System.out.print("Enter your choice here: ");
+                    playerChoice = scanner.nextInt();
+                }
+                System.out.println("\nDiscarded card " + hand.get(playerChoice-1) + " from your hand.\n");
+                discardCard(hand,(playerChoice));
+        }
+    }
+
+    protected void discardCard(List<DtoCardResponse> card, int index){
+        card.remove((index-1));
     }
 
     public String getName() {
