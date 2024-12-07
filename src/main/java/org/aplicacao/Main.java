677c968ce22ee491596346eafee99dc964b6398c
robertomalatestale <robertomalatestale@gmail.com>
2024-12-06 22:08:34 -0300

    Implement a fullGame (BO3 matches) method and logic

--- src/main/java/org/aplicacao/Main.java
+++ src/main/java/org/aplicacao/Main.java
@@ -5,8 +5,8 @@ public class Main {
         Match match = new Match();
         Player player1 = new Player("Roberto");
         Player player2 = new Player("Davi");
-        match.gameMatch(player1,player2);
+        match.fullGame(player1,player2);
     }
 }
 
-//TODO Put a printf to tells wich players is current move. Implements game match counter
+//TODO Need to correct a bug with discard mechanic
--- src/main/java/org/aplicacao/Match.java
+++ src/main/java/org/aplicacao/Match.java
@@ -2,7 +2,8 @@ package org.aplicacao;
 
 public class Match {
     private int matchNumber = 0;
-    private char matchResult;
+    private int player1MatchesWon;
+    private int player2MatchesWon;
     private DeckOfCards deck = new DeckOfCards();
     private String deckId;
     {
@@ -14,16 +15,42 @@ public class Match {
     }
 
     public void gameMatch(Player player1, Player player2) throws Exception {
+        System.out.println("MATCH " + (matchNumber+1));
         if (matchNumber == 0){
             player1.fulfillHand(deckId,3);
             player2.fulfillHand(deckId,3);
-            matchNumber += 1;
         }
+        matchNumber += 1;
         int matchResult = GameLogic.highCardBattle(player1.playerTurn(deckId),player2.playerTurn(deckId));
         if (matchResult == 1){
+            this.player1MatchesWon +=1;
             System.out.println(GameLogic.announceWinner(matchResult,player1.getName()));
-        } else {
+        } else if (matchResult == 2){
+            this.player2MatchesWon +=1;
             System.out.println(GameLogic.announceWinner(matchResult,player2.getName()));
+        }else {
+            System.out.println(GameLogic.announceWinner(matchResult,player2.getName()));
+        }
+    }
+
+    public void fullGame(Player player1, Player player2) throws Exception {
+        gameMatch(player1,player2);
+        gameMatch(player1,player2);
+        if (GameLogic.verifyWinner(player1.getName(),this.player1MatchesWon)){
+            System.out.println("GAME RESULT: " + player1.getName() + " " + this.player1MatchesWon + " - " + this.player2MatchesWon + " " + player2.getName());
+        } else if (GameLogic.verifyWinner(player2.getName(),this.player2MatchesWon)){
+            System.out.println("GAME RESULT: " + player1.getName() + " " + this.player1MatchesWon + " - " + this.player2MatchesWon + " " + player2.getName());
+        } else {
+            gameMatch(player1,player2);
+            if (GameLogic.verifyWinner(player1.getName(),this.player1MatchesWon)){
+                System.out.println("GAME RESULT: " + player1.getName() + " " + this.player1MatchesWon + " - " + this.player2MatchesWon + " " + player2.getName());
+            } else if (GameLogic.verifyWinner(player2.getName(),this.player2MatchesWon)){
+                System.out.println("GAME RESULT: " + player1.getName() + " " + this.player1MatchesWon + " - " + this.player2MatchesWon + " " + player2.getName());
+            }
+        }
+        if (this.player1MatchesWon == this.player2MatchesWon){
+            System.out.println("\nTHE GAME WAS A DRAW!");
+            System.out.println("GAME RESULT: " + player1.getName() + " " + this.player1MatchesWon + " - " + this.player2MatchesWon + " " + player2.getName());
         }
     }
 }
